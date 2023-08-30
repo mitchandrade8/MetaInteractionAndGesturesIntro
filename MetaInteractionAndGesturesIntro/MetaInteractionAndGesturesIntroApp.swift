f7bf372191fdc429de63f1f6c6f07f18a701570a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-30 16:12:27 -0600

    Initial Commit

---
+++ MetaInteractionAndGesturesIntro/MetaInteractionAndGesturesIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaInteractionAndGesturesIntroApp.swift
+//  MetaInteractionAndGesturesIntro
+//
+//  Created by Mitch Andrade on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaInteractionAndGesturesIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
